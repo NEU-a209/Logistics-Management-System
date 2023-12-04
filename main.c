0c06d9560423c36435570fea33c3836a1d44c21f
Icbitic <icbitic@foxmail.com>
2023-12-05 01:08:59 +0800

    some updates

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
-//
-// Created by Kalen Suen on 2023/12/4.
-//
+/*
+ * THIS FILE IS CREATED UNDER THE COOPERATION OF NEU-a209.
+ * GITHUB ORGANIZATION LINK:
+ * https://github.com/NEU-a209
+ *
+ * GITHUB REPOSITORY LINK:
+ * https://github.com/NEU-a209/Logistics-Management-System
+ *
+ * Created by Kalen Suen on 2023/12/04.
+ *
+ * CODE PRINCIPLES:
+ * 1. NO MAGIC NUMBERS.
+ * 2. DECLARATIONS AND DEFINITIONS SHOULD BE SPLIT.
+ * 3. PROPER INDENTS.
+ * 4. SPACE REQUIRED AROUND OPERATORS.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
+
 
 #define MAX_REPOSITORY_NAME_LENGTH  64
 #define MAX_REPOSITORY_CONNECTIONS  16
@@ -15,6 +31,10 @@
 #define MAX_REPOSITORY_NUM          512
 
 #define TYPE_NUM 3
+
+#define INITIAL_INDEX        0
+#define DEFAULT_PAUSING_MODE true
+
 
 enum TYPE {
     NORMAL,
@@ -30,6 +50,7 @@ enum OPERATION {
     ADD_REPOSITORY,
     REMOVE_REPOSITORY,
     PRINT_ALL_REPOSITORY,
+    TOGGLE_PAUSING,
     EXIT_PROGRAM
 };
 
@@ -38,6 +59,7 @@ enum ERROR_CODE {
     ERR_ITEM_NOT_FOUND,
     ERR_NO_CONNECTION_FOUND
 };
+
 
 struct Repository;
 struct Connection;
@@ -73,6 +95,14 @@ struct Item {
 
 void printMenu();
 
+void printAllItems();
+
+void printAllRepositories();
+
+struct Item constructItem();
+
+struct Repository constructRepository();
+
 int addItem(struct Item item);
 
 int removeItem(struct Item *item);
@@ -89,42 +119,74 @@ int removeRepositoryByIndex(int repositoryIndex);
 
 struct Repository *getRepositoryByIndex(int repositoryIndex);
 
+void pauseProgram();
+
 
 struct Item items[MAX_ITEM_NUM];
 struct Repository repositories[MAX_REPOSITORY_NUM];
 
-int currentItemIndex = 0;
-int currentRepositoryIndex = 0;
+int currentItemIndex = INITIAL_INDEX;
+int currentRepositoryIndex = INITIAL_INDEX;
+
 
 int main(void) {
     bool isExitTriggered = false;
+    bool isPausingRequired = DEFAULT_PAUSING_MODE;
     while (!isExitTriggered) {
         printMenu();
 
         int operation;
         scanf("%d", &operation); // NOLINT(*-err34-c)
 
+        struct Item item;
+        struct Repository repository;
+
+        struct Item *itemPointer;
+        struct Repository *repositoryPointer;
+
+        int itemIndex;
+        int repositoryIndex;
+
         switch (operation) {
             case ADD_ITEM:
+                item = constructItem();
+                itemIndex = addItem(item);
                 break;
             case REMOVE_ITEM:
+                scanf("%d", &itemIndex); // NOLINT(*-err34-c)
+                itemPointer = getItemByIndex(itemIndex);
+                removeItem(itemPointer);
                 break;
             case MODIFY_ITEM:
+                scanf("%d", &itemIndex); // NOLINT(*-err34-c)
+                item = constructItem();
+                *getItemByIndex(itemIndex) = item;
                 break;
             case PRINT_ALL_ITEMS:
+                printAllItems();
                 break;
             case EXIT_PROGRAM:
                 isExitTriggered = true;
                 break;
             case ADD_REPOSITORY:
+                repository = constructRepository();
+                repositoryIndex = addRepository(repository);
                 break;
             case REMOVE_REPOSITORY:
+                scanf("%d", &repositoryIndex); // NOLINT(*-err34-c)
+                repositoryPointer = getRepositoryByIndex(repositoryIndex);
+                removeRepository(repositoryPointer);
                 break;
             case PRINT_ALL_REPOSITORY:
+                printAllRepositories();
+                break;
+            case TOGGLE_PAUSING:
+                isPausingRequired = !isPausingRequired;
                 break;
             default:
                 break;
         }
+        if (isPausingRequired) pauseProgram();
     }
 }
 
