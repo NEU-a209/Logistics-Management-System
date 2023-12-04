08182a17cd837548d2e27e702a630528c18a057b
Icbitic <icbitic@foxmail.com>
2023-12-05 00:36:15 +0800

    Basic Framework

--- main.c
+++ main.c
@@ -2,54 +2,138 @@
 // Created by Kalen Suen on 2023/12/4.
 //
 #include <stdio.h>
-/*主界面
-添加货物
-查看货物，
-删除货物
-修改货物
-货物出库
-两个注意事项:1.订单不存在问题.2.订单重复问题
-*/
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+
+#define MAX_REPOSITORY_NAME_LENGTH  64
+#define MAX_REPOSITORY_CONNECTIONS  16
+#define MAX_ITEM_NAME_LENGTH        64
+#define MAX_STORAGE_INFO_NUM        16
+#define MAX_ITEM_NUM                8192
+#define MAX_REPOSITORY_NUM          512
+
+#define TYPE_NUM 3
+
+enum TYPE {
+    NORMAL,
+    FRAGILE,
+    REFRIGERATION
+};
+
+enum OPERATION {
+    ADD_ITEM,
+    REMOVE_ITEM,
+    MODIFY_ITEM,
+    PRINT_ALL_ITEMS,
+    ADD_REPOSITORY,
+    REMOVE_REPOSITORY,
+    PRINT_ALL_REPOSITORY,
+    EXIT_PROGRAM
+};
+
+enum ERROR_CODE {
+    ERR_REPOSITORY_NOT_FOUND,
+    ERR_ITEM_NOT_FOUND,
+    ERR_NO_CONNECTION_FOUND
+};
+
+struct Repository;
+struct Connection;
+struct StorageInfo;
+struct Item;
+
+struct Connection {
+    double transportationTime[TYPE_NUM];
+    struct Repository *destination;
+};
+
+struct Repository {
+    int index;
+    char name[MAX_REPOSITORY_NAME_LENGTH];
+    struct Connection connections[MAX_REPOSITORY_CONNECTIONS];
+};
+
+struct StorageInfo {
+    struct Repository *repository;
+    int timeIn;
+    int timeOut;
+};
+
+struct Item {
+    int index;
+    int timeCreated;
+    char name[MAX_ITEM_NAME_LENGTH];
+    int type;
+    struct Repository *currentRepository;
+    struct StorageInfo storageInfo[MAX_STORAGE_INFO_NUM];
+};
 
 
-/*
-typedef struct
-{
-    char id[15];
-    char 
-    
-} goods;*/
+void printMenu();
+
+int addItem(struct Item item);
+
+int removeItem(struct Item *item);
+
+int removeItemByIndex(int itemIndex);
+
+struct Item *getItemByIndex(int itemIndex);
+
+int addRepository(struct Repository repository);
+
+int removeRepository(struct Repository *repository);
+
+int removeRepositoryByIndex(int repositoryIndex);
+
+struct Repository *getRepositoryByIndex(int repositoryIndex);
+
+
+struct Item items[MAX_ITEM_NUM];
+struct Repository repositories[MAX_REPOSITORY_NUM];
+
+int currentItemIndex = 0;
+int currentRepositoryIndex = 0;
 
 int main(void) {
-    int operation1;
-    printf("欢迎来到物流信息管理系统……\n");
-    printf("1 添加货物\n");
-    printf("2 删除货物\n");
-    printf("3 修改货物\n");
-    printf("4 查看所有货物\n");
-    printf("5 \n");
-    printf("请选择需要的操作：\n");
+    bool isExitTriggered = false;
+    while (!isExitTriggered) {
+        printMenu();
 
-    scanf("%d", &operation1);
-}
-/*
-    switch(operation1)
-    {
-        case 1:
-            add();
-            break;
-        case 2:
-            delete();
-            break;
-        case 3:
-            modify();
-            break;
-        case 4:
-            check();
-            break;
-        case 5:
-            
+        int operation;
+        scanf("%d", &operation); // NOLINT(*-err34-c)
 
-
+        switch (operation) {
+            case ADD_ITEM:
+                break;
+            case REMOVE_ITEM:
+                break;
+            case MODIFY_ITEM:
+                break;
+            case PRINT_ALL_ITEMS:
+                break;
+            case EXIT_PROGRAM:
+                isExitTriggered = true;
+                break;
+            case ADD_REPOSITORY:
+                break;
+            case REMOVE_REPOSITORY:
+                break;
+            case PRINT_ALL_REPOSITORY:
+                break;
+            default:
+                break;
+        }
     }
-    */
+}
+
+void printMenu() {
+    printf("欢迎来到物流信息管理系统……\n");
+    printf("1: 添加货物\n");
+    printf("2: 删除货物\n");
+    printf("3: 修改货物\n");
+    printf("4: 查看所有货物\n");
+    printf("5: 退出程序\n");
+    printf("请选择需要的操作：\n");
+}
